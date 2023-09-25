6b71f756b51950ef8bcfca02e821f4b635baf1d8
Ngala Mac <macngala4@gmail.com>
2023-09-25 09:40:01 +0100

    Create animals table in schema.sql

--- schema.sql
+++ schema.sql
@@ -1,5 +1,9 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+/* SQL command to create the animals table. */
+CREATE TABLE animals(id INT NOT NULL primary key,
+                     name VARCHAR(250),
+                     date_of_birth DATE, 
+                     escape_attempts INT, 
+                     neutered BOOLEAN, 
+                     weight_kg FLOAT);
