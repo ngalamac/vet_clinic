9730a5610e2be99a4f7ba45a60daaa970a794854
Ngala Mac <macngala4@gmail.com>
2023-09-26 11:07:49 +0100

    populate more data

--- data.sql
+++ data.sql
@@ -11,3 +11,25 @@ VALUES (3, 'Pikachu', '2021-01-07', 1, FALSE, 15.04);
 
 INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES (4, 'Devimon', '2017-05-12', 5, TRUE, 11);
+
+/* Populate more data after adding the species column. */
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg, species)
+VALUES (5, 'Charmander', '2020-02-08', 0, FALSE, -11, NULL);
+
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg, species)
+VALUES (6, 'Plantmon', '2021-11-15', 2, TRUE, -5.7, NULL);
+
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg, species)
+VALUES (7, 'Squirtle', '1993-04-02', 3, FALSE, -12.13, NULL);
+
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg, species)
+VALUES (8, 'Angemon', '2005-06-12', 1, TRUE, -45, NULL);
+
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg, species)
+VALUES (9, 'Boarmon', '2005-06-07', 7, TRUE, 20.4, NULL);
+
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg, species)
+VALUES (10, 'Blossom', '1998-10-13', 3, TRUE, 17, NULL);
+
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg, species)
+VALUES (11, 'Ditto', '2022-05-14', 4, TRUE, 22, NULL);
