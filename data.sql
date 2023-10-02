236cae4804335e9d8882a491c83cc34b584a77b9
Ian Kiema <112932127+iankiema@users.noreply.github.com>
2023-10-02 16:52:40 +0300

    Update data.sql  to populate more data.

--- data.sql
+++ data.sql
@@ -97,3 +97,5 @@ FROM (
 ) vet_ids,
 generate_series('1980-01-01'::timestamp, '9999-01-01', '4 hours') visit_timestamp
 ON CONFLICT DO NOTHING;
+-- This will add 2.500.000 owners with full_name = 'Owner <X>' and email = 'owner_<X>@email.com' (~2min approx.)
+INSERT INTO owners (full_name, email) SELECT 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
